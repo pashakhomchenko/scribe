e515c0ab98c454dd7bbe24aab0933195df2c4a4b
Pasha Khomchenko <pavel.kh005@gmail.com>
2023-07-12 09:43:40 -0700

    update meta

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -7,6 +7,7 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
 import { PageMeta } from '../types';
+import { getURL } from '@/utils/helpers';
 
 interface Props extends PropsWithChildren {
   meta?: PageMeta;
@@ -17,7 +18,7 @@ export default function Layout({ children, meta: pageMeta }: Props) {
   const meta = {
     title: 'Scribe',
     description: 'Intelligent summaries for your meetings. In one click.',
-    cardImage: '',
+    cardImage: 'scribe_banner.png',
     ...pageMeta
   };
 
@@ -31,17 +32,12 @@ export default function Layout({ children, meta: pageMeta }: Props) {
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🖋️</text></svg>"
         />
         <meta content={meta.description} name="description" />
-        <meta
-          property="og:url"
-          content={`https://subscription-starter.vercel.app${router.asPath}`}
-        />
+        <meta property="og:url" content={`${getURL()}${router.asPath}`} />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.cardImage} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@vercel" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.cardImage} />
