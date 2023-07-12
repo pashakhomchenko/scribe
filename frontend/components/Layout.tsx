a7ccacc1ae2868721226d643452ca39c431f7edf
Pasha Khomchenko <pavel.kh005@gmail.com>
2023-07-12 10:38:30 -0700

    another attempt to fix twitter preview

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -19,6 +19,7 @@ export default function Layout({ children, meta: pageMeta }: Props) {
     title: 'Scribe',
     description: 'Intelligent summaries for your meetings. In one click.',
     cardImage: '/scribe_banner.png',
+    url: "https://tryscribe.world"
     ...pageMeta
   };
 
@@ -32,15 +33,15 @@ export default function Layout({ children, meta: pageMeta }: Props) {
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🖋️</text></svg>"
         />
         <meta content={meta.description} name="description" />
-        <meta property="og:url" content={getURL()} />
+        <meta property="og:url" content={meta.url} />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.cardImage} />
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:site" content="@tryscribeai" />
         <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content={getURL()} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.cardImage} />
       </Head>
